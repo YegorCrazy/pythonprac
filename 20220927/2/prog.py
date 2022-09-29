6a197a4f8878a1421e53d7be9228d08318422ec4
YegorCrazy <egor2002288@gmail.com>
2022-09-29 22:21:08 +0300

    second task added

--- 20220927/2/prog.py
+++ 20220927/2/prog.py
@@ -3,13 +3,11 @@ def key (x):
 
 l = list(eval(input()))
 
-flag = True
-while flag:
+flag = False
+while not flag:
     flag = True
     for i in range(len(l) - 1):
         if key(l[i]) > key(l[i + 1]):
             l[i], l[i + 1] = l[i + 1], l[i]
             flag = False
-    if flag:
-        break
 print(l)
