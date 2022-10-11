12818983090a0b2bdcda3e708f4f762a2d937fe8
YegorCrazy <egor2002288@gmail.com>
2022-10-11 15:39:12 +0300

    first task added

--- 20221011/1/prog.py
+++ 20221011/1/prog.py
@@ -1,13 +1,29 @@
 from fractions import Fraction
 
+def count_fr (args, start, s):
+    fr = args[start]
+    for i in range(int(args[start - 1])):
+        fr *= s
+        fr += args[start + i + 1]
+    return fr
+
+def is_root (args):
+    s = args[0]
+    w = args[1]
+
+    start = 3
+    fr_1 = count_fr(args, start, s)
+    start = start + int(args[2]) + 2
+    fr_2 = count_fr(args, start, s)
+
+    return fr_1 / fr_2 == w
+
 args = input().split(", ")
 new_args = []
 for x in args:
     new_args.append(Fraction(str(x)))
 
-s = new_args[0]
-w = new_args[1]
+print(is_root(new_args))
 
-fr_1 = Fraction(1)
-for i in range(new_args[2]):
-    
+
+
