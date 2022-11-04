243557eb297c5f9b8ed5186164a3b3f23735edcb
YegorCrazy <egor2002288@gmail.com>
2022-11-05 01:48:21 +0300

    second task added

---
+++ 20221101/3/prog.py
@@ -0,0 +1,19 @@
+import math
+
+class Grange:
+    def __init__(self, b0, q, bn):
+        self.b0 = b0
+        self.q = q
+        self.bn = bn
+    def __len__(self):
+        return math.ceil(math.log(self.bn / self.b0, self.q))
+    def __iter__(self):
+        return iter([self.b0 * (self.q ** n) for n in range(len(self))])
+    def __getitem__(self, index):
+        if type(index) == type(slice(1)):
+            return Grange(index.start, self.q if index.step == None else self.q ** index.step, index.stop)
+        return self.b0 * (self.q ** index)
+    def __str__(self):
+        return f'grande({self.b0}, {self.q}, {self.bn})'
+    def __repr__(self):
+        return f'grande({self.b0}, {self.q}, {self.bn})'
