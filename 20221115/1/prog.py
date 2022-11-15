a9fe29d03a38c136d1c1b5532a8dbef4d0af0fa9
YegorCrazy <egor2002288@gmail.com>
2022-11-15 12:00:23 +0300

    first task added

---
+++ 20221115/1/prog.py
@@ -0,0 +1,24 @@
+def objcount(cls):
+    cls.counter = 0
+    def new_init(self, *args, **kwargs):
+        self.__class__.counter += 1
+        return self.__init(*args, **kwargs)
+    cls.__init = cls.__init__
+    cls.__init__ = new_init
+    def new_del(self, *args, **kwargs):
+        self.__class__.counter -= 1
+        return self.__del(*args, **kwargs)
+    def new_old_del(self):
+        pass
+    cls.__del = cls.__del__ if '__del__' in dir(cls) else new_old_del
+    cls.__del__ = new_del
+    return cls
+
+@objcount
+class C:
+    pass
+
+c, d, e = C(), C(), C()
+print(C.counter)
+c = 100500
+print(C.counter)
